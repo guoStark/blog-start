d0b546d639980a8182209e6aa92aa97b617c8b09
guojunhang <1364020251@qq.com>
2018-11-28 18:30:47 +0800

    first commit

---
+++ src/main/java/com/first/blog/blogstart/BlogStartApplication.java
@@ -0,0 +1,12 @@
+package com.first.blog.blogstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogStartApplication.class, args);
+	}
+}
